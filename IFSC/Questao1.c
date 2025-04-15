18d6659e72d0ddf5a9efd334424960836aa6f843
Hiluky <odaisantos48@gmail.com>
2025-04-15 09:18:06 -0300

    Update Questao1.c

--- IFSC/Questao1.c
+++ IFSC/Questao1.c
@@ -1,1 +1,16 @@
+Questao 7
 
+#include <stdio.h>
+
+int main() {
+    float raio, area;
+
+    printf("Digite o raio: ");
+    scanf("%f", &raio);
+
+    area = 3.14159 * raio * raio;
+
+    printf("A área da circunferência é: %.2f\n", area);
+
+    return 0;
+}
