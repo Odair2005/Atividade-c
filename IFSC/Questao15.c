ae215c1204cf93043129c3aca53dac809163a656
Hiluky <odaisantos48@gmail.com>
2025-04-15 12:01:29 -0300

    Create Questao15.c

---
+++ IFSC/Questao15.c
@@ -0,0 +1,16 @@
+Questao21
+
+#include <stdio.h>
+
+int main() {
+    int A, B, resultado;
+
+    printf("Digite dois valores inteiros: ");
+    scanf("%d %d", &A, &B);
+
+    resultado = (A + B) * (A + B);
+
+    printf("Quadrado da soma: %d\n", resultado);
+
+    return 0;
+}
