1ac843365445828a17251e2ad818ebbbccf426a2
Hiluky <odaisantos48@gmail.com>
2025-05-01 14:01:23 -0300

    Create Questao25.c

---
+++ IFSC/Questao25.c
@@ -0,0 +1,13 @@
+Questão 31
+  
+#include <stdio.h>
+int main() {
+ int numero;
+ printf("Digite um número: ");
+ scanf("%d", &numero);
+ if (numero % 2 == 0)
+ printf("Par");
+ else
+ printf("Ímpar");
+ return 0;
+}
